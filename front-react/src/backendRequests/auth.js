33685c82837bc01a63e74e0058dae5092ca288b0
AmirMohammad <amirmohammadghadimi8@gmail.com>
2022-02-06 20:02:35 +0330

    cleared the loges for debugging

--- front-react/src/backendRequests/auth.js
+++ front-react/src/backendRequests/auth.js
@@ -6,11 +6,9 @@ const handleLoginSubmit = async (
   signedUpLocation,
   loggedInLocation
 ) => {
-  console.log("handleLoginSubmit");
   if (e.target[0].value.trim() === "") {
     return;
   }
-  console.log("handleLoginSubmit");
 
   const data = new FormData(e.target);
   data.append("csrf_token", await get_csrf_token());
@@ -24,21 +22,14 @@ const handleLoginSubmit = async (
 
   fetch("/auth/login/", requestOptions)
     .then((response) => {
-      console.log(response);
       if (response.ok) {
-        console.log("handleLoginSubmit");
-
         return response.json();
       } else {
         throw new Error("Error logging in");
       }
     })
     .then((data) => {
-      console.log("handleLoginSubmit");
-      console.log(data);
       if (data.success === true) {
-        console.log("handleLoginSubmit");
-
         if (data.status === "signed-up") {
           redirectCallback(signedUpLocation);
         } else if (data.status === "logged-in") {
